15bbfc4b46cf8aa56d7e5a096f486a8fefb8cc8f
pazz <patricktotzke@gmail.com>
2011-05-29 12:10:40 +0100

    pretty colour defaults for 256c mode

--- settings.py
+++ settings.py
@@ -4,36 +4,41 @@ terminal_cmd = 'urxvt -T notmuch -e %s'
 spawn_editor = True
 spawn_pager = True
 
+# colour palette.
+# id, fg16, bg16, mono, fg256, bg256
+# see http://excess.org/urwid/reference.html#AttrSpec
+# http://excess.org/urwid/wiki/DisplayAttributes
+# interactive test-palette: http://excess.org/urwid/browser/palette_test.py
 palette = [
-    ('header', 'white', 'dark blue', 'bold', '', ''),
-    ('footer', 'white', 'dark blue', 'bold', '', ''),
-    ('prompt', 'light gray', 'black', '', '', ''),
-    ('threadline', 'light gray', '', '', '', ''),
-    ('threadline_focus', 'white', 'dark gray', '', '', ''),
+    ('header', 'white', 'dark blue', 'bold', 'white', 'dark blue'),
+    ('footer', 'white', 'dark blue', 'bold,standout', 'white', '#006'),
+    ('prompt', 'light gray', 'black', 'standout', 'light gray', ''),
+    ('threadline', '', '', '', '', ''),
+    ('threadline_date', 'light gray', '', '', 'g58', ''),
+    ('threadline_mailcount', 'light gray', '', '', 'light gray', ''),
+    ('threadline_tags', 'brown', '', '', '#a86', ''),
+    ('threadline_authors', 'dark green', '', '', '#6d6', ''),
+    ('threadline_subject', 'light gray', '', '', 'g58', ''),
+    ('threadline_content', 'dark gray', '', '', '#866', ''),
+    ('threadline_focus', 'white', 'dark gray', 'standout', 'white', 'g11'),
+    ('threadline_date_linefocus', 'light gray', 'dark gray', 'standout', 'g58', 'g11'),
+    ('threadline_mailcount_linefocus', 'light gray', 'dark gray', 'standout', 'light gray', 'g11'),
+    ('threadline_tags_linefocus', 'yellow,bold', 'dark gray', 'standout', '#ff8', 'g11'),
+    ('threadline_authors_linefocus', 'dark green,bold', 'dark gray', 'standout','#8d6', 'g11'),
+    ('threadline_subject_linefocus', 'light gray', 'dark gray', 'standout','g58', 'g11'),
 
-    ('threadline_date', 'light gray', '', '', '', ''),
-    ('threadline_mailcount', 'light gray', '', '', '', ''),
-    ('threadline_tags', 'yellow', '', '', '', ''),
-    ('threadline_authors', 'dark green', '', '', '', ''),
-    ('threadline_subject', 'light gray', '', '', '', ''),
-    ('threadline_date_linefocus', 'light gray', 'dark gray', '', '', ''),
-    ('threadline_mailcount_linefocus', 'light gray', 'dark gray', '', '', ''),
-    ('threadline_tags_linefocus', 'yellow,bold', 'dark gray', '', '', ''),
-    ('threadline_authors_linefocus', 'dark green', 'dark gray', '', '', ''),
-    ('threadline_subject_linefocus', 'light gray', 'dark gray', '', '', ''),
+    ('messagesummary_even', 'white', 'light blue', 'standout', 'white', '#068'),
+    ('messagesummary_odd', 'white', 'dark blue', 'standout', 'white', '#006'),
+    ('messagesummary_focus', 'white', 'black', 'standout,bold', '', ''),
+    ('message_header', 'white', 'dark gray', '', 'white', 'dark gray'),
+    ('message_body', 'light gray', 'black', '', 'light gray', ''),
 
-    ('messagesummary_even', 'white', 'light blue', '', '', ''),
-    ('messagesummary_odd', 'white', 'dark blue', '', '', ''),
-    ('messagesummary_focus', 'white', 'black', '', '', ''),
-    ('message_header', 'white', 'dark gray', '', '', ''),
-    ('message_body', 'light gray', 'black', '', '', ''),
-
-    ('bufferlist_results_even', 'light gray', 'black', '', '', ''),
+    ('bufferlist_results_even', 'light gray', 'black', '', '', 'g3'),
     ('bufferlist_results_odd', 'light gray', 'black', '', '', ''),
-    ('bufferlist_focus', 'white', 'dark gray', '', '#ffa', ''),
+    ('bufferlist_focus', 'white', 'dark gray', '', '#ffa', 'g38'),
 
     ('taglist_tag', 'light gray', 'black', '', '', ''),
-    ('taglist_focus', 'white', 'dark gray', '', '', ''),
+    ('taglist_focus', 'white', 'dark gray', '', '#ffa', 'g38'),
 ]
 displayed_headers = [
     'From',
