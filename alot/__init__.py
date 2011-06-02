aaa0dac422f2e75299fed1d332eb59617348a067
pazz <patricktotzke@gmail.com>
2011-06-02 10:37:10 +0100

    setup.py and friends

---
+++ alot/__init__.py
@@ -0,0 +1,10 @@
+__all__ = ['alot']
+
+__productname__ = 'alot'
+__version__     = "0.0.1"
+__copyright__   = "Copyright (C) 2011 Patrick Totzke"
+__author__      = "Patrick Totzke"
+__author_email__= "patricktotzke@gmx.de"
+__description__ = "Terminal User Interface for notmuch mail (notmuchmail.org)"
+__homepage__ = "https://github.com/pazz/notmuch-gui"
+__license__  = "Licensed under the GNU GPL v3+."
