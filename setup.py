995319432ccbef138f060e152073a22656681bfc
pazz <patricktotzke@gmail.com>
2011-08-13 15:55:27 +0100

    pep8 and dependencies in setup.py
    issue #43

--- setup.py
+++ setup.py
@@ -3,15 +3,16 @@
 from distutils.core import setup
 import alot
 
-setup(name = "alot",
-      version = alot.__version__,
-      description = alot.__description__,
-      author = alot.__author__,
-      author_email = alot.__author_email__,
-      url = alot.__url__,
-      packages = ['alot'],
-      scripts = ['bin/alot'],
-      license = alot.__copyright__,
-      install_requires = ['notmuch', 'argparse' 'urwid'],
+setup(name='alot',
+      version=alot.__version__,
+      description=alot.__description__,
+      author=alot.__author__,
+      author_email=alot.__author_email__,
+      url=alot.__url__,
+      packages=['alot'],
+      scripts=['bin/alot'],
+      license=alot.__copyright__,
+      requires=['notmuch (>=0.7.1)', 'argparse', 'urwid (>=0.9.9.1)'],
+      provides='alot'
 )
 
