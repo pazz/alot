c12df8fa48f56e3f55e2cdc4299126d06aa80d79
Patrick Totzke <patricktotzke@gmail.com>
2011-10-15 12:21:03 +0100

    utf8 fixes after shlex

--- alot/commands/__init__.py
+++ alot/commands/__init__.py
@@ -76,7 +76,7 @@ def commandfactory(cmdline, mode='global'):
         return None
     logging.debug('mode:%s got commandline "%s"' % (mode, cmdline))
     args = shlex.split(cmdline.encode('utf-8'))
-    args = filter(lambda x: x.decode('utf-8'), args)
+    args = map(lambda x: x.decode('utf-8'), args)  # get unicode strings
     logging.debug('ARGS: %s' % args)
     cmdname = args[0]
     args = args[1:]
@@ -128,48 +128,9 @@ def commandfactory(cmdline, mode='global'):
 #        return commandfactory(cmd, mode=mode, tagsstring=params)
 #    elif cmd == 'shellescape':
 #        return commandfactory(cmd, mode=mode, commandstring=params)
-#    elif cmd == 'set':
-#        key, value = params.split(' ', 1)
-#        return commandfactory(cmd, mode=mode, key=key, value=value)
-#    elif cmd == 'toggletag':
-#        return commandfactory(cmd, mode=mode, tags=params.split())
-#    elif cmd == 'fold':
-#        return commandfactory(cmd, mode=mode, all=(params == '--all'))
-#    elif cmd == 'unfold':
-#        return commandfactory(cmd, mode=mode, all=(params == '--all'))
-#    elif cmd == 'save':
-#        args = params.split(' ')
-#        allset = False
-#        pathset = None
-#        if args:
-#            if args[0] == '--all':
-#                allset = True
-#                pathset = ' '.join(args[1:])
-#            else:
-#                pathset = params
-#        return commandfactory(cmd, mode=mode, all=allset, path=pathset)
 #    elif cmd == 'edit':
 #        filepath = os.path.expanduser(params)
 #        if os.path.isfile(filepath):
 #            return commandfactory(cmd, mode=mode, path=filepath)
-#    elif cmd == 'print':
-#        args = [a.strip() for a in params.split()]
-#        return commandfactory(cmd, mode=mode,
-#                              whole_thread=('--thread' in args),
-#                              separately=('--separately' in args))
-#    elif cmd == 'pipeto':
-#        return commandfactory(cmd, mode=mode, command=params)
-#
-#    elif not params and cmd in ['exit', 'flush', 'pyshell', 'taglist',
-#                                'bclose', 'compose', 'openfocussed',
-#                                'closefocussed', 'bnext', 'bprevious', 'retag',
-#                                'refresh', 'bufferlist', 'refineprompt',
-#                                'reply', 'open', 'groupreply', 'bounce',
-#                                'openthread', 'toggleheaders', 'send',
-#                                'cancel', 'reedit', 'select', 'retagprompt']:
-#        return commandfactory(cmd, mode=mode)
-#    else:
-#        return None
-
 
 __all__ = list(filename[:-3] for filename in glob.glob1(os.path.dirname(__file__), '*.py'))
