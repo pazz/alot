b6ceef6d757b02162219c05ac9557df442cb6d6d
Patrick Totzke <patricktotzke@gmail.com>
2011-11-16 15:42:09 +0000

    adjusted release version

--- alot/__init__.py
+++ alot/__init__.py
@@ -1,8 +1,8 @@
 __productname__ = 'alot'
-__version__ = '0.11-dev'
+__version__ = '0.20'
 __copyright__ = "Copyright (C) 2011 Patrick Totzke"
 __author__ = "Patrick Totzke"
 __author_email__ = "patricktotzke@gmail.com"
-__description__ = "Command-line MUA using notmuch mail"
+__description__ = "Terminal MUA using notmuch mail"
 __url__ = "https://github.com/pazz/alot"
 __license__ = "Licensed under the GNU GPL v3+."
