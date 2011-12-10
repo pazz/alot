eada26f35ee24bbb71d1fbbba8f6f78f09954da1
Patrick Totzke <patricktotzke@gmail.com>
2011-12-10 14:18:52 +0000

    docstrings for search and bufferlist commands

--- alot/commands/bufferlist.py
+++ alot/commands/bufferlist.py
@@ -3,15 +3,17 @@ from alot.commands import Command, registerCommand
 MODE = 'bufferlist'
 
 
-@registerCommand(MODE, 'select', help='focus selected buffer')
+@registerCommand(MODE, 'select')
 class BufferFocusCommand(Command):
+    """focus selected buffer"""
     def apply(self, ui):
         selected = ui.current_buffer.get_selected_buffer()
         ui.buffer_focus(selected)
 
 
-@registerCommand(MODE, 'close', help='close focussed buffer')
+@registerCommand(MODE, 'close')
 class BufferCloseCommand(Command):
+    """close focussed buffer"""
     def apply(self, ui):
         bufferlist = ui.current_buffer
         selected = bufferlist.get_selected_buffer()
