57e1489063523f9b118783ced53bb8d4a6b09ad0
Patrick Totzke <patricktotzke@gmail.com>
2011-12-19 17:24:41 +0000

    initial option parsing with subcommands
    via twisted.python.usage.Options instead of argparse
    issue #132

--- alot/init.py
+++ alot/init.py
@@ -14,42 +14,68 @@ from commands import *
 from alot.commands import CommandParseError
 import alot
 
+from twisted.python import usage
+class SearchOptions(usage.Options):
+    def parseArgs(self, *args):
+        self['query'] = ' '.join(args)
 
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-c', dest='configfile',
-                        default=None,
-                        help='alot\'s config file')
-    parser.add_argument('-n', dest='notmuchconfigfile',
-                        default='~/.notmuch-config',
-                        help='notmuch\'s config file')
-    parser.add_argument('-C', dest='colours',
-                        type=int,
-                        choices=[1, 16, 256],
-                        help='colour mode')
-    parser.add_argument('-r', dest='read_only',
-                        action='store_true',
-                        help='open db in read only mode')
-    parser.add_argument('-p', dest='db_path',
-                        help='path to notmuch index')
-    parser.add_argument('-d', dest='debug_level',
-                        default='info',
-                        choices=['debug', 'info', 'warning', 'error'],
-                        help='debug level')
-    parser.add_argument('-l', dest='logfile',
-                        default='/dev/null',
-                        help='logfile')
-    parser.add_argument('command', nargs='?',
-                        default='',
-                        help='initial command')
-    parser.add_argument('-v', '--version', action='version',
-                        version='%(prog)s ' + alot.__version__)
-    return parser.parse_args()
+
+class ComposeOptions(usage.Options):
+    optParameters = [
+                ['sender', '', None, 'From line'],
+                ['subject', '', None, 'subject line'],
+                ['cc', '', None, 'copy to'],
+                ['bcc', '', None, 'blind copy to'],
+                ['template', '', None, 'path to template file'],
+            ]
+    def parseArgs(self, *args):
+        self['to'] = ' '.join(args)
+
+    def as_argparse_opts(self):
+        optstr = ''
+        for k, v in self.items():
+            if v is not None:
+                optstr += '--%s \'%s\' ' % (k, v)
+        return optstr
+
+class Options(usage.Options):
+    optFlags = [
+            ["read-only", "r", 'open db in read only mode'],
+            ]
+    def colourint(val):
+        val = int(val)
+        if val not in [1, 16, 256]:
+            raise ValueError("Not in range")
+        return val
+    colourint.coerceDoc = "Must be 1, 16 or 256"
+    optParameters = [
+            ['config', 'c', '~/.config/alot/config', 'config file'],
+            ['notmuch-config', 'n', '~/.notmuch-config', 'notmuch config'],
+            ['colour-mode', 'C', 256, 'terminal colour mode', colourint],
+            ['mailindex-path', 'p', None, 'path to notmuch index'],
+            ['debug-level', 'd', 'info', 'debug level used in combination with -l'],
+            ['logfile', 'l', '/dev/null', 'logfile'],
+            ]
+    subCommands = [['search', None, SearchOptions, "search for threads"],
+                   ['compose', None, ComposeOptions, "compose a message"]]
+
+
+    def opt_version(self):
+        print alot.__version__
+        sys.exit(0)
+
 
 
 def main():
     # interpret cml arguments
-    args = parse_args()
+
+    args = Options()
+    try:
+        args.parseOptions() # When given no argument, parses sys.argv[1:]
+    except usage.UsageError, errortext:
+        print '%s: %s' % (sys.argv[0], errortext)
+        print '%s: Try --help for usage details.' % (sys.argv[0])
+        sys.exit(1)
 
     # locate alot config files
     configfiles = [
@@ -58,14 +84,14 @@ def main():
                      'alot', 'config'),
         os.path.expanduser('~/.alot.rc'),
     ]
-    if args.configfile:
-        expanded_path = os.path.expanduser(args.configfile)
+    if args['config']:
+        expanded_path = os.path.expanduser(args['config'])
         if not os.path.exists(expanded_path):
             sys.exit('File %s does not exist' % expanded_path)
         configfiles.insert(0, expanded_path)
 
     # locate notmuch config
-    notmuchfile = os.path.expanduser(args.notmuchconfigfile)
+    notmuchfile = os.path.expanduser(args['notmuch-config'])
 
     try:
         # read the first alot config file we find
@@ -81,8 +107,8 @@ def main():
         sys.exit(e)
 
     # setup logging
-    numeric_loglevel = getattr(logging, args.debug_level.upper(), None)
-    logfilename = os.path.expanduser(args.logfile)
+    numeric_loglevel = getattr(logging, args['debug-level'].upper(), None)
+    logfilename = os.path.expanduser(args['logfile'])
     logging.basicConfig(level=numeric_loglevel, filename=logfilename)
     logger = logging.getLogger()
 
@@ -91,12 +117,17 @@ def main():
     aman = AccountManager(settings.config)
 
     # get ourselves a database manager
-    dbman = DBManager(path=args.db_path, ro=args.read_only)
+    dbman = DBManager(path=args['mailindex-path'], ro=args['read-only'])
 
     # get initial searchstring
     try:
-        if args.command != '':
-            cmd = commands.commandfactory(args.command, 'global')
+        if args.subCommand == 'search':
+            query = args.subOptions['query']
+            cmd = commands.commandfactory('search ' + query, 'global')
+        elif args.subCommand == 'compose':
+            cmdstring = 'compose %s' % args.subOptions.as_argparse_opts()
+            print cmdstring
+            cmd = commands.commandfactory(cmdstring, 'global')
         else:
             default_commandline = settings.config.get('general',
                                                       'initial_command')
@@ -109,7 +140,7 @@ def main():
        logger,
        aman,
        cmd,
-       args.colours,
+       args['colour-mode'],
     )
 
 if __name__ == "__main__":
