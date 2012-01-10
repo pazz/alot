b757c83ec2c25ee5643b3b818ce4578bf5ceca5d
Patrick Totzke <patricktotzke@gmail.com>
2012-01-10 17:41:44 +0000

    Theme user manual

--- docs/user/source/conf.py
+++ docs/user/source/conf.py
@@ -96,14 +96,14 @@ html_theme = 'default'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {'nosidebar': True}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+html_title = 'Alot User Manual'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
--- docs/user/source/index.rst
+++ docs/user/source/index.rst
@@ -3,18 +3,14 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to alot's documentation!
-================================
+User Manual
+===========
 
 Contents:
 
 .. toctree::
    :maxdepth: 2
 
-Indices and tables
-==================
-
-* :ref:`genindex`
-* :ref:`modindex`
-* :ref:`search`
-
+   usage
+   config
+   theming
