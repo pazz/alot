6b71780934b32f832f75f45c0de708db9cf7c61a
Patrick Totzke <patricktotzke@gmail.com>
2012-01-14 15:10:36 +0000

    added module for crypto helpers

---
+++ alot/crypto.py
@@ -0,0 +1,41 @@
+from helper import call_cmd
+import tempfile
+import os
+
+
+#field descriptors for output of `gpg --with-colons`
+keys = [
+    'type', 'trust', 'length', 'algorithm', 'key_id', 'creation_date',
+    'expiration_date', 'serial_number', 'ownertrust', 'user_id',
+    'signature_class', 'capabilities', 'cfingerprint', 'flag',
+    'token_serial_number'
+]
+
+
+#def get_gpg_output(arglist):
+#    out, err, rval = call_cmd(['gpg', '--with-colons'] + arglist)
+#    keydicts = []
+#    for line in out.strip().split('\n'):
+#        keydicts.append(dict(zip(keys, line.split(':'))))
+#    return keydicts
+#
+#
+#def get_secret_keys():
+#    return get_gpg_output(['--list-secret-keys'])
+#
+#
+#def get_keys():
+#    return get_gpg_output(['--list--keys'])
+
+
+def verify(blob, sig):
+    """call gnupg to verify content string blob agains signature string sig"""
+    sigfile = tempfile.NamedTemporaryFile(delete=False, suffix='.sig')
+    sigfile.write(sig)
+    sigfile.close()
+
+    out, err, rval = call_cmd(['gpg', '--verify', sigfile.name, '-'],
+                              stdin=blob)
+
+    os.unlink(sigfile.name)
+    return err, rval
