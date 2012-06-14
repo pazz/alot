f6f0a8facdbc09a7d1bed12f4ef4b151f9fa3bf7
Patrick Totzke <patricktotzke@gmail.com>
2012-06-14 22:17:12 +0100

    cleanup docstring for BooleanAction

--- alot/utils/booleanaction.py
+++ alot/utils/booleanaction.py
@@ -1,19 +1,13 @@
 # Copyright (C) 2011-2012  Patrick Totzke <patricktotzke@gmail.com>
 # This file is released under the GNU GPL, version 3 or a later revision.
 # For further details see the COPYING file
-"""
-This defines the ConfigureAction for argparse found here:
-http://code.google.com/p/argparse/issues/detail?id=2#c6
-
-We use it to set booelan arguments for command parameters
-to False using a `--no-` prefix.
-"""
 import argparse
 import re
 
 
 TRUEISH = ['1', 't', 'true', 'yes', 'on']
 FALSISH = ['0', 'f', 'false', 'no', 'off']
+
 
 def boolean(string):
     string = string.lower()
@@ -26,6 +20,9 @@ def boolean(string):
 
 
 class BooleanAction(argparse.Action):
+    """
+    argparse action that can be used to store boolean values
+    """
     def __init__(self, *args, **kwargs):
         kwargs['type'] = boolean
         kwargs['choices'] = TRUEISH + FALSISH
