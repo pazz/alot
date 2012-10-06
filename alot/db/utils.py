f8224b417fabb833a7db71f63422474a9ac199b5
Patrick Totzke <patricktotzke@gmail.com>
2012-10-06 20:27:44 +0100

    add db.util.mimeparse
    This will transform a given email.Message into a tree of
    parts: each part-dict contains some meta info, its interpretation as
    unicode string (via handlers) and a list `children` that points further
    down the tree.
    Note that this interpretation is done asyncronously and thus also returns
    a Defered.
    Later on, this will be responsible for decryption or verification of the mail.

--- alot/db/utils.py
+++ alot/db/utils.py
@@ -11,6 +11,8 @@ charset.add_charset('utf-8', charset.QP, charset.QP, 'utf-8')
 from email.iterators import typed_subpart_iterator
 import logging
 import mailcap
+from twisted.internet import defer
+from twisted.internet.threads import deferToThread
 
 import alot.helper as helper
 from alot.settings import settings
@@ -32,7 +34,7 @@ def extract_headers(mail, headers=None):
     :type headers: list of str
     """
     headertext = u''
-    if headers == None:
+    if headers is None:
         headers = mail.keys()
     for key in headers:
         value = u''
@@ -177,3 +179,113 @@ def encode_header(key, value):
     else:
         value = Header(value)
     return value
+
+
+def interpret(part):
+    ctype = part.get_content_type()
+    enc = part.get_content_charset() or 'ascii'
+    raw_payload = part.get_payload(decode=True)
+    #get mime handler
+    key = 'copiousoutput'
+    handler, entry = settings.mailcap_find_match(ctype, key=key)
+
+    if entry:
+        # open tempfile, respect mailcaps nametemplate
+        nametemplate = entry.get('nametemplate', '%s')
+        prefix, suffix = parse_mailcap_nametemplate(nametemplate)
+        tmpfile = tempfile.NamedTemporaryFile(delete=False, prefix=prefix,
+                                              suffix=suffix)
+        # write payload to tmpfile
+        tmpfile.write(raw_payload)
+        tmpfile.close()
+
+        # read parameter, create handler command
+        parms = tuple(map('='.join, part.get_params()))
+
+        # create and call external command
+        cmd = mailcap.subst(entry['view'], ctype,
+                            filename=tmpfile.name, plist=parms)
+        logging.debug('command: %s' % cmd)
+        logging.debug('parms: %s' % str(parms))
+        cmdlist = split_commandstring(cmd)
+        # call handler
+        rendered_payload, errmsg, retval = helper.call_cmd(cmdlist)
+        # remove tempfile
+        os.unlink(tmpfile.name)
+        if rendered_payload:  # handler had output
+            return string_decode(rendered_payload, enc)
+
+
+def mimeparse(part):
+    ctype = part.get_content_type()
+    maintype = part.get_content_maintype()
+    subtype = part.get_content_subtype()
+    cdisp = part.get('Content-Disposition', '')
+    logging.debug('read part: %s' % ctype)
+    tree = {
+        'ctype': ctype,
+        'children': [],
+        'email': part,
+        'attachment': cdisp.startswith('attachment')
+    }
+    d = defer.Deferred()
+    dlist = []
+
+    def addchild(child):
+        tree['children'].append(child)
+
+    if ctype == 'multipart/encrypted':
+        # A multipart/encrypted message has two parts. The first part has
+        # control information that is needed to decrypt the
+        # application/octet-stream second part. Similar to signed messages,
+        # there are different implementations which are identified by their
+        # separate content types for the control part. The most common types
+        # are "application/pgp-encrypted" (RFC 3156) and
+        # "application/pkcs7-mime" (S/MIME). Defined in RFC 1847, Section
+        # 2.2
+        # TODO: decrypt here, parse plaintext to email.Message, recur on that.
+        pass
+    elif ctype == 'multipart/signed':
+        # A multipart/signed message is used to attach a digital signature
+        # to a message. It has two parts, a body part and a signature part.
+        # The whole of the body part, including mime headers, is used to
+        # create the signature part. Many signature types are possible, like
+        # application/pgp-signature (RFC 3156) and
+        # application/pkcs7-signature (S/MIME). Defined in RFC 1847,
+        # Section 2.1
+        # TODO: define function that verifies sig and stores result, call it in thread and recur
+        for subpart in part.get_payload():
+            mimeparse(subpart).addCallback(addchild)
+    elif ctype == 'message/rfc822':
+        logging.debug('rfc822')
+        # A message/rfc822 part contains an email message, including any
+        # headers. This is used for digests as well as for email
+        # forwarding. Defined in RFC 2046.
+        pass
+
+    elif maintype == 'multipart':
+        for subpart in part.get_payload():
+            ld = mimeparse(subpart)
+            ld.addCallback(addchild)
+            dlist.append(ld)
+    else:
+        enc = part.get_content_charset() or 'ascii'
+        raw_payload = part.get_payload(decode=True)
+        raw_payload = string_decode(raw_payload, enc)
+
+        if ctype == 'text/plain':
+            tree['content'] = raw_payload
+        else:
+            dl = deferToThread(interpret, part)
+
+            def add_content(data):
+                tree['content'] = data
+
+            dl.addCallback(add_content)
+            dlist.append(dl)
+
+    def ret(foo):
+        d.callback(tree)
+    dl = defer.DeferredList(dlist)
+    dl.addCallback(ret)
+    return d
