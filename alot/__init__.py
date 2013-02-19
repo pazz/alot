ecf5a504019b8ed57a903332e6f256225519297e
Patrick Totzke <patricktotzke@gmail.com>
2013-02-19 10:15:51 +0000

    bumped version string

--- alot/__init__.py
+++ alot/__init__.py
@@ -1,6 +1,6 @@
 __productname__ = 'alot'
-__version__ = '0.3.3+'
-__copyright__ = "Copyright (C) 2012 Patrick Totzke"
+__version__ = '0.3.4'
+__copyright__ = "Copyright (C) 2013 Patrick Totzke"
 __author__ = "Patrick Totzke"
 __author_email__ = "patricktotzke@gmail.com"
 __description__ = "Terminal MUA using notmuch mail"
