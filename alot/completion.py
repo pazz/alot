d72f1f42990b5e36d26ff5df0ea949084492d226
Patrick Totzke <patricktotzke@gmail.com>
2013-03-03 12:51:40 +0000

    completion for move command

--- alot/completion.py
+++ alot/completion.py
@@ -426,7 +426,7 @@ class CommandCompleter(Completer):
                     res = self._pathcompleter.complete(params, localpos)
                 elif self.mode == 'envelope' and cmd in ['sign', 'togglesign']:
                     res = self._secretkeyscompleter.complete(params, localpos)
-                elif self.mode == 'envelope' and cmd in ['encrypt', 
+                elif self.mode == 'envelope' and cmd in ['encrypt',
                                                          'rmencrypt',
                                                          'toggleencrypt']:
                     res = self._publickeyscompleter.complete(params, localpos)
@@ -438,6 +438,14 @@ class CommandCompleter(Completer):
                     localcomp = MultipleSelectionCompleter(self._tagcompleter,
                                                            separator=',')
                     res = localcomp.complete(params, localpos)
+                elif cmd == 'move':
+                    directions = ['up', 'down', 'page up', 'page down']
+                    if self.mode == 'thread':
+                        directions += ['first', 'next', 'previous',
+                                       'last reply', 'first reply', 'parent',
+                                       'next sibling','previous sibling']
+                    localcompleter = StringlistCompleter(directions)
+                    res = localcompleter.complete(params, localpos)
 
                 # prepend cmd and correct position
                 res = [('%s %s' % (cmd, t), p + len(cmd) +
