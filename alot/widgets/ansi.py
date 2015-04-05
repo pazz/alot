fa381b3f101c582cafb69570e95c94255815a746
Patrick Totzke <patricktotzke@gmail.com>
2015-04-05 13:12:31 +0100

    setting to ignore ANSI escapes for backgrounds
    The new setting "interpret_ansi_background" (boolean, defaults to
    False), allows to disable interpretation of ANSI escape characters that
    change the background colour of text.

--- alot/widgets/ansi.py
+++ alot/widgets/ansi.py
@@ -17,7 +17,7 @@ class ANSIText(urwid.WidgetWrap):
                                                default_attr_focus,
                                                ansi_background)
         t = urwid.Text(ct, **kwds)
-        attr_map = {default_attr.background: ''}
+        attr_map = { default_attr.background: ''}
         w = urwid.AttrMap(t, attr_map, focus_map)
         urwid.WidgetWrap.__init__(self, w)
 
