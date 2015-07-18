52cef182e0047735a5d6fc9ba409e5436d1ea293
Thomas Levine <_@thomaslevine.com>
2015-07-18 10:59:52 -0400

    copyright

--- alot/helper.py
+++ alot/helper.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2011-2012  Patrick Totzke <patricktotzke@gmail.com>
+# Copyright (C) 2015 Thomas Levine <_@thomaslevine.com>
 # This file is released under the GNU GPL, version 3 or a later revision.
 # For further details see the COPYING file
 from datetime import timedelta
