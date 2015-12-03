0ad2d8aa11275239aa121fb08c602e2e80683213
Christian Geier <geier@lostpackets.de>
2015-12-04 00:29:59 +0100

    wrapping 2 iterators in list(), sorting them later

--- alot/db/thread.py
+++ alot/db/thread.py
@@ -152,8 +152,8 @@ class Thread(object):
         if self._authors is None:
             seen = {}
             msgs = self.get_messages().keys()
-            msgs_with_date = filter(lambda m: m.get_date() is not None, msgs)
-            msgs_without_date = filter(lambda m: m.get_date() is None, msgs)
+            msgs_with_date = list(filter(lambda m: m.get_date() is not None, msgs))
+            msgs_without_date = list(filter(lambda m: m.get_date() is None, msgs))
             # sort messages with date and append the others
             msgs_with_date.sort(None, lambda m: m.get_date())
             msgs = msgs_with_date + msgs_without_date
