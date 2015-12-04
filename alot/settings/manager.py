7e633200a23866da95ec9d1dc544ec74247af5c6
Christian Geier <geier@lostpackets.de>
2015-12-04 12:36:56 +0100

    better treatment of issue causing filter iterators

--- alot/settings/manager.py
+++ alot/settings/manager.py
@@ -7,6 +7,7 @@ import re
 import mailcap
 import logging
 from configobj import ConfigObj, Section
+from itertools import chain
 
 from alot.account import SendmailAccount
 from alot.addressbook.abook import AbookAddressBook
@@ -320,7 +321,7 @@ class SettingsManager(object):
             prefixs = prefix + ' '
             cand = filter(lambda x: x.startswith(prefixs), candidates)
             if prefix in candidates:
-                candidates = cand + [prefix]
+                candidates = chain(cand, [prefix])
             else:
                 candidates = cand
         return candidates
