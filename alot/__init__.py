4d06b9e5361358f009c573008c2ba714f85b0049
Lucas Hoffmann <l-m-h@web.de>
2015-12-10 10:18:50 +0100

    Fix version number for development.
    Setuptools gave an error message and asked to conform to PEP440.
    https://www.python.org/dev/peps/pep-0440/#developmental-releases

--- alot/__init__.py
+++ alot/__init__.py
@@ -1,5 +1,5 @@
 __productname__ = 'alot'
-__version__ = '0.3.6+'
+__version__ = '0.3.7.dev1'
 __copyright__ = "Copyright (C) 2014 Patrick Totzke"
 __author__ = "Patrick Totzke"
 __author_email__ = "patricktotzke@gmail.com"
