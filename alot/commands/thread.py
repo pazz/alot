00c7d41e80b24aa7a146440ce7bd996df4358a64
Lucas Hoffmann <l-m-h@web.de>
2016-12-11 13:27:15 +0100

    Use native logging string interpolation
    This will also make the string interpolation lazy evaluated.

--- alot/commands/thread.py
+++ alot/commands/thread.py
@@ -82,8 +82,7 @@ def determine_sender(mail, action='reply'):
                 regex = re.compile('^' + alias + '$', flags=re.IGNORECASE)
                 for seen_name, seen_address in candidate_addresses:
                     if regex.match(seen_address):
-                        logging.debug("match!: '%s' '%s'" % (seen_address,
-                                                             alias))
+                        logging.debug("match!: '%s' '%s'", seen_address, alias)
                         if settings.get(action + '_force_realname'):
                             realname = account.realname
                         else:
