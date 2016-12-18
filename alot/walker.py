237499e631741dbb3837e5ef99cc66f842e8a92e
Lucas Hoffmann <l-m-h@web.de>
2016-12-18 10:35:05 +0100

    Turn alot.walker.PipeWalker.focus into a property

--- alot/walker.py
+++ alot/walker.py
@@ -16,18 +16,20 @@ class PipeWalker(urwid.ListWalker):
         self.kwargs = kwargs
         self.containerclass = containerclass
         self.lines = []
-        self.focus = 0
+        self._focus = 0
         self.empty = False
         self.direction = -1 if reverse else 1
 
     def __contains__(self, name):
         return self.lines.__contains__(name)
 
-    def get_focus(self):
-        return self._get_at_pos(self.focus)
+    @property
+    def focus(self):
+        return self._get_at_pos(self._focus)
 
-    def set_focus(self, focus):
-        self.focus = focus
+    @focus.setter
+    def focus(self, value):
+        self._focus = value
         self._modified()
 
     def get_next(self, start_from):
@@ -37,9 +39,9 @@ class PipeWalker(urwid.ListWalker):
         return self._get_at_pos(start_from - self.direction)
 
     def remove(self, obj):
-        next_focus = self.focus % len(self.lines)
-        if self.focus == len(self.lines) - 1 and self.empty:
-            next_focus = self.focus - 1
+        next_focus = self._focus % len(self.lines)
+        if self._focus == len(self.lines) - 1 and self.empty:
+            next_focus = self._focus - 1
 
         self.lines.remove(obj)
         if self.lines:
