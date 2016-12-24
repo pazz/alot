26fb82dce0be42ab175c2c87037ec3e439f4ee49
Patrick Totzke <patricktotzke@gmail.com>
2016-12-24 11:50:10 +0000

    typo

--- tests/db/thread_test.py
+++ tests/db/thread_test.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-"""Tests for the alt.db.thread module."""
+"""Tests for the alot.db.thread module."""
 
 import datetime
 import unittest
