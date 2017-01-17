aba3e5c26b409008ed3dcf585770b8e41e34a8bf
Lucas Hoffmann <l-m-h@web.de>
2017-01-17 10:09:52 +0100

    Port old doctests to unittest: alot.settings.checks

---
+++ tests/settings/checks_test.py
@@ -0,0 +1,16 @@
+# encoding=utf-8
+
+import unittest
+
+from alot.settings import checks
+
+
+class TestForceList(unittest.TestCase):
+
+    def test_strings_are_converted_to_single_item_lists(self):
+        forced = checks.force_list('hello')
+        self.assertEqual(forced, ['hello'])
+
+    def test_empty_strings_are_converted_to_empty_lists(self):
+        forced = checks.force_list('')
+        self.assertEqual(forced, [])
