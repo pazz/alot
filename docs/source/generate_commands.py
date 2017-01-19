24098a4f8a33262ca94e994842b8e2c0ed99f86b
Dylan Baker <dylan@pnwbakers.com>
2017-01-19 11:09:25 -0800

    fixups

--- docs/source/generate_commands.py
+++ docs/source/generate_commands.py
@@ -17,6 +17,7 @@ import textwrap as _textwrap
 
 NOTE = ".. CAUTION: THIS FILE IS AUTO-GENERATED!\n\n\n"
 
+
 class HF(HelpFormatter):
     def _metavar_formatter(self, action, default_metavar):
         if action.metavar is not None:
@@ -33,67 +34,68 @@ class HF(HelpFormatter):
 
 
 def rstify_parser(parser):
-        #header = parser.format_usage().strip()
-        #print '\n\n%s\n' % header + '_' * len(header)
-        parser.formatter_class = HF
-        #parser.print_help()
-        #continue
+    #header = parser.format_usage().strip()
+    #print '\n\n%s\n' % header + '_' * len(header)
+    parser.formatter_class = HF
+    #parser.print_help()
+    #continue
 
-        formatter = parser._get_formatter()
-        out = ""
+    formatter = parser._get_formatter()
+    out = ""
 
-        # usage
-        usage = formatter._format_usage(None, parser._actions,
-                                         parser._mutually_exclusive_groups,
-                                        '').strip()
-        usage = usage.replace('--','---')
+    # usage
+    usage = formatter._format_usage(None, parser._actions,
+                                     parser._mutually_exclusive_groups,
+                                    '').strip()
+    usage = usage.replace('--', '---')
 
-        # section header
-        out += '.. describe:: %s\n\n' % parser.prog
+    # section header
+    out += '.. describe:: %s\n\n' % parser.prog
 
-        # description
-        out += ' '*4 + parser.description
+    # description
+    out += ' '*4 + parser.description
+    out += '\n\n'
+
+    if len(parser._positionals._group_actions) == 1:
+        out += "    argument\n"
+        a = parser._positionals._group_actions[0]
+        out += ' '*8 + str(parser._positionals._group_actions[0].help)
+        if a.choices:
+            out += ". valid choices are: %s." % ','.join(['\`%s\`' % s for s
+                                                          in a.choices])
+        if a.default:
+            out += ". defaults to: '%s'." % a.default
         out += '\n\n'
-
-        if len(parser._positionals._group_actions) == 1:
-            out += "    argument\n"
-            a = parser._positionals._group_actions[0]
-            out += ' '*8 + str(parser._positionals._group_actions[0].help)
+    elif len(parser._positionals._group_actions) > 1:
+        out += "    positional arguments\n"
+        for index, a in enumerate(parser._positionals._group_actions):
+            out += "        %s: %s" % (index, a.help)
             if a.choices:
                 out += ". valid choices are: %s." % ','.join(['\`%s\`' % s for s
                                                               in a.choices])
             if a.default:
                 out += ". defaults to: '%s'." % a.default
-            out += '\n\n'
-        elif len(parser._positionals._group_actions) > 1:
-            out += "    positional arguments\n"
-            for index, a in enumerate(parser._positionals._group_actions):
-                out += "        %s: %s" % (index, a.help)
-                if a.choices:
-                    out += ". valid choices are: %s." % ','.join(['\`%s\`' % s for s
-                                                                  in a.choices])
-                if a.default:
-                    out += ". defaults to: '%s'." % a.default
-                out += '\n'
-            out += '\n\n'
+            out += '\n'
+        out += '\n\n'
 
-        if parser._optionals._group_actions:
-            out += "    optional arguments\n"
-        for a in parser._optionals._group_actions:
-            switches = [s.replace('--','---') for s in a.option_strings]
-            out += "        :%s: %s" % (', '.join(switches), a.help)
-            if a.choices and not isinstance(a, BooleanAction):
-                out += ". Valid choices are: %s" % ','.join(['\`%s\`' % s for s
-                                                              in a.choices])
-            if a.default:
-                out += " (Defaults to: '%s')" % a.default
-            out += '.\n'
-        out += '\n'
+    if parser._optionals._group_actions:
+        out += "    optional arguments\n"
+    for a in parser._optionals._group_actions:
+        switches = [s.replace('--', '---') for s in a.option_strings]
+        out += "        :%s: %s" % (', '.join(switches), a.help)
+        if a.choices and not isinstance(a, BooleanAction):
+            out += ". Valid choices are: %s" % ','.join(['\`%s\`' % s for s
+                                                         in a.choices])
+        if a.default:
+            out += " (Defaults to: '%s')" % a.default
+        out += '.\n'
+    out += '\n'
 
-        # epilog
-        #out += formatter.add_text(parser.epilog)
+    # epilog
+    #out += formatter.add_text(parser.epilog)
 
-        return out
+    return out
+
 
 def get_mode_docs():
     docs = {}
@@ -107,26 +109,32 @@ def get_mode_docs():
 
 def generate_command_tables():
     modes = []
-    for mode, modecommands in COMMANDS.items():
-        modefilename = mode+'.rst'
-        modefile = open(os.path.join(HERE, 'usage', 'modes', modefilename), 'w')
-        modefile.write(NOTE)
-        if mode != 'global':
-            modes.append(mode)
-            header = 'Commands in `%s` mode' % mode
-            modefile.write('%s\n%s\n' % (header, '-' * len(header)))
-            modefile.write('The following commands are available in %s mode\n\n' % mode)
-        else:
-            header = 'Global Commands'
-            modefile.write('%s\n%s\n' % (header, '-' * len(header)))
-            modefile.write('The following commands are available globally\n\n')
-        for cmdstring,struct in modecommands.items():
-            cls, parser, forced_args = struct
-            labelline = '.. _cmd.%s.%s:\n\n' % (mode, cmdstring.replace('_',
-                                                                        '-'))
-            modefile.write(labelline)
-            modefile.write(rstify_parser(parser))
-        modefile.close()
+    base = os.path.join(HERE, 'usage', 'modes')
+    os.makedirs(base)
+    for mode, modecommands in COMMANDS.iteritems():
+        modefilename = mode + '.rst'
+        with open(os.path.join(base, modefilename), 'w') as modefile:
+            modefile.write(NOTE)
+            if mode != 'global':
+                modes.append(mode)
+                header = 'Commands in `{}` mode'.format(mode)
+                modefile.write('{}\n{}\n'.format(header, '-' * len(header)))
+                modefile.write(
+                    'The following commands are available in {}'
+                    'mode\n\n'.format(mode))
+            else:
+                header = 'Global Commands'
+                modefile.write('{}\n{}\n'.format(header, '-' * len(header)))
+                modefile.write(
+                    'The following commands are available globally\n\n')
+
+            for cmdstring, struct in modecommands.iteritems():
+                parser = struct[1]
+                labelline = '.. _cmd.{}.{}:\n\n'.format(
+                    mode, cmdstring.replace('_', '-'))
+                modefile.write(labelline)
+                modefile.write(rstify_parser(parser))
+
 
 if __name__ == "__main__":
     generate_command_tables()
