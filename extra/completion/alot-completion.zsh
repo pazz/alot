87c8c8a46b613b149a2bc9440dd4567604e3c0eb
Lucas Hoffmann <l-m-h@web.de>
2017-01-24 11:45:39 +0100

    Update zsh completion after #944

--- extra/completion/alot-completion.zsh
+++ extra/completion/alot-completion.zsh
@@ -2,7 +2,7 @@
 
 # ZSH completion for `alot`, Shamelessly copied from notmuch's zsh completion file
 # Copyright © 2009 Ingmar Vanhassel <ingmar@exherbo.org>
-# Copyright © 2012 Patrick Totzke <patricktotzke@gmail.com>
+# Copyright © 2012-2017 Patrick Totzke <patricktotzke@gmail.com>
 
 _alot_subcommands()
 {
@@ -10,6 +10,9 @@ _alot_subcommands()
   alot_subcommands=(
     'search:search for messages matching the search terms, display matching threads as results'
     'compose:compose a new message'
+    'bufferlist:show a list of open alot buffers'
+    'taglist:list all tags in the database'
+    'pyshell:start the interactive python shell inside alot'
   )
 
   _describe -t command 'command' alot_subcommands
@@ -40,15 +43,15 @@ _alot()
   # Complete global options.  Set $state to "command" or "options" in order to
   # do further completion.
   _arguments \
-    '(- *)--help[show the help message]' \
-    '(- *)--version[show version information]' \
-    '(--help --version -r --read-only)'-{r,-read-only}'[open db in read only mode]' \
-    '(--help --version -c --config)'-{c,-config}'[specify an alternative config file]:alot config file:_files' \
-    '(--help --version -n --notmuch-config)'-{n,-notmuch-config}'[specify an alternative notmuch config file]:notmuch config file:_files' \
-    '(--help --version -C --colour-mode)'-{C,-colour-mode}'[terminal colour mode]:colour mode:(1 16 256)' \
-    '(--help --version -p --mailindex-path)'-{p,-mailindex-path}'[path to notmuch index]:directory:_directories' \
-    '(--help --version -d --debug-level)'-{d,-debug-level}'[set the log level]:debug level:(debug info warning error)' \
-    '(--help --version -l --logfile)'-{l,-logfile}'[specify the logfile (default: /dev/null)]:log file:_files' \
+    '(- *)'{-h,--help}'[show the help message]' \
+    '(- *)'{-v,--version}'[show version information]' \
+    '(-h --help -v --version -r --read-only)'-{r,-read-only}'[open db in read only mode]' \
+    '(-h --help -v --version -c --config)'-{c,-config}'[specify an alternative config file]:alot config file:_files' \
+    '(-h --help -v --version -n --notmuch-config)'-{n,-notmuch-config}'[specify an alternative notmuch config file]:notmuch config file:_files' \
+    '(-h --help -v --version -C --colour-mode)'-{C,-colour-mode}'[terminal colour mode]:colour mode:(1 16 256)' \
+    '(-h --help -v --version -p --mailindex-path)'-{p,-mailindex-path}'[path to notmuch index]:directory:_directories' \
+    '(-h --help -v --version -d --debug-level)'-{d,-debug-level}'[set the log level]:debug level:(debug info warning error)' \
+    '(-h --help -v --version -l --logfile)'-{l,-logfile}'[specify the logfile (default: /dev/null)]:log file:_files' \
     ': :->command' \
     '*:: :->options' \
   && ret=0
