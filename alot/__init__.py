0ffc9a0f81f503212bd1c58004f8c9b9f7d6892c
Dylan Baker <dylan@pnwbakers.com>
2017-02-09 09:21:53 -0800

    alot: fix dev version

--- alot/__init__.py
+++ alot/__init__.py
@@ -1,5 +1,5 @@
 __productname__ = 'alot'
-__version__ = '0.5.1'
+__version__ = '0.6.0dev'
 __copyright__ = "Copyright (C) 2012-17 Patrick Totzke"
 __author__ = "Patrick Totzke"
 __author_email__ = "patricktotzke@gmail.com"
