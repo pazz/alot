4a808dad4cdf6ff2668cbec3a8750f6a6551593e
Dylan Baker <dylan@pnwbakers.com>
2017-08-17 09:20:24 -0700

    setup.py: Reindent
    This normalizes the indent of the file to be much more readable.

--- setup.py
+++ setup.py
@@ -4,38 +4,42 @@ from setuptools import setup, find_packages
 import alot
 
 
-setup(name='alot',
-      version=alot.__version__,
-      description=alot.__description__,
-      author=alot.__author__,
-      author_email=alot.__author_email__,
-      url=alot.__url__,
-      license=alot.__copyright__,
-      packages=find_packages(exclude=['tests*']),
-      package_data={'alot': [
-                             'defaults/alot.rc.spec',
-                             'defaults/notmuch.rc.spec',
-                             'defaults/abook_contacts.spec',
-                             'defaults/default.theme',
-                             'defaults/default.bindings',
-                             'defaults/config.stub',
-                             'defaults/theme.spec',
-                            ]},
-      entry_points={
-          'console_scripts':
-              ['alot = alot.__main__:main'],
-      },
-      install_requires=[
+setup(
+    name='alot',
+    version=alot.__version__,
+    description=alot.__description__,
+    author=alot.__author__,
+    author_email=alot.__author_email__,
+    url=alot.__url__,
+    license=alot.__copyright__,
+    packages=find_packages(exclude=['tests*']),
+    package_data={
+        'alot': [
+            'defaults/alot.rc.spec',
+            'defaults/notmuch.rc.spec',
+            'defaults/abook_contacts.spec',
+            'defaults/default.theme',
+            'defaults/default.bindings',
+            'defaults/config.stub',
+            'defaults/theme.spec',
+        ]
+    },
+    entry_points={
+        'console_scripts':
+            ['alot = alot.__main__:main'],
+    },
+    install_requires=[
         'notmuch>=0.13',
         'urwid>=1.1.0',
         'urwidtrees>=1.0',
         'twisted>=10.2.0',
         'python-magic',
         'configobj>=4.7.0',
-        'gpg'],
-      tests_require=[
-          'mock',
-      ],
-      provides=['alot'],
-      test_suite="tests",
-      )
+        'gpg'
+    ],
+    tests_require=[
+        'mock',
+    ],
+    provides=['alot'],
+    test_suite="tests",
+)
