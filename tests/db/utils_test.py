ce076b14bc2c221492f3f80e6d5159d627f0eba0
Dylan Baker <dylan@pnwbakers.com>
2018-02-26 11:04:28 -0800

    Fix some of the quoted words tests for py3k

--- tests/db/utils_test.py
+++ tests/db/utils_test.py
@@ -6,6 +6,7 @@
 from __future__ import absolute_import
 
 import base64
+import codecs
 import email
 import email.header
 import email.mime.application
@@ -244,10 +245,10 @@ class TestDecodeHeader(unittest.TestCase):
         :rtype: str
         """
         string = unicode_string.encode(encoding)
-        output = '=?' + encoding + '?Q?'
+        output = b'=?' + encoding.encode('ascii') + b'?Q?'
         for byte in string:
-            output += '=' + byte.encode('hex').upper()
-        return output + '?='
+            output += b'=' + codecs.encode(bytes([byte]), 'hex').upper()
+        return output + b'?='
 
     @staticmethod
     def _base64(unicode_string, encoding):
@@ -305,17 +306,17 @@ class TestDecodeHeader(unittest.TestCase):
 
     def test_quoted_words_can_be_interrupted(self):
         part = u'ÄÖÜäöü'
-        text = self._base64(part, 'utf-8') + ' and ' + \
+        text = self._base64(part, 'utf-8') + b' and ' + \
             self._quote(part, 'utf-8')
         expected = u'ÄÖÜäöü and ÄÖÜäöü'
         self._test(text, expected)
 
     def test_different_encodings_can_be_mixed(self):
         part = u'ÄÖÜäöü'
-        text = 'utf-8: ' + self._base64(part, 'utf-8') + \
-            ' again: ' + self._quote(part, 'utf-8') + \
-            ' latin1: ' + self._base64(part, 'iso-8859-1') + \
-            ' and ' + self._quote(part, 'iso-8859-1')
+        text = b'utf-8: ' + self._base64(part, 'utf-8') + \
+            b' again: ' + self._quote(part, 'utf-8') + \
+            b' latin1: ' + self._base64(part, 'iso-8859-1') + \
+            b' and ' + self._quote(part, 'iso-8859-1')
         expected = u'utf-8: ÄÖÜäöü again: ÄÖÜäöü latin1: ÄÖÜäöü and ÄÖÜäöü'
         self._test(text, expected)
 
