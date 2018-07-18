4035958005f57bc617e3067e0b35c7409a32be33
Dylan Baker <dylan@pnwbakers.com>
2018-07-17 21:27:46 -0700

    utilities: Add a decorator for asyncio tests
    This decorator works to allow tests for pure asyncio coroutines to
    operate synchronously.

--- tests/utilities.py
+++ tests/utilities.py
@@ -16,6 +16,7 @@
 
 """Helpers for unittests themselves."""
 
+import asyncio
 import functools
 import unittest
 
@@ -186,3 +187,14 @@ def expected_failure(func):
     """
     func.todo = 'expected failure'
     return func
+
+
+def async_test(coro):
+    """Run an asyncrounous test synchronously."""
+
+    @functools.wraps(coro)
+    def _actual(*args, **kwargs):
+        loop = asyncio.get_event_loop()
+        return loop.run_until_complete(coro(*args, **kwargs))
+
+    return _actual
