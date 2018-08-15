726731fbf694504d259615037ede4309551f00c7
Lucas Hoffmann <l-m-h@web.de>
2018-08-15 16:53:31 +0200

    Update python requirements to 3.6
    This is needed as we use functions from the stdlib mock module that were
    only added in 3.6.

--- setup.py
+++ setup.py
@@ -21,7 +21,6 @@ setup(
             'License :: OSI Approved'
             ':: GNU General Public License v3 or later (GPLv3+)'),
         'Operating System :: POSIX',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3 :: Only',
@@ -55,5 +54,5 @@ setup(
     ],
     provides=['alot'],
     test_suite="tests",
-    python_requires=">=3.5",
+    python_requires=">=3.6",
 )
