52f92e891a09796ef4212ed355e8ce0faede9d79
Dylan Baker <dylan@pnwbakers.com>
2018-10-04 08:46:22 -0700

    ui: Use Eventloop.create_future instead of asyncio.Future
    This is recommended since other implementations can use this hook to
    return their own Future compatible implementations.

--- alot/ui.py
+++ alot/ui.py
@@ -310,7 +310,7 @@ class UI(object):
         """
         history = history or []
 
-        fut = asyncio.Future()
+        fut = asyncio.get_event_loop().create_future()
         oldroot = self.mainloop.widget
 
         def select_or_cancel(text):
@@ -539,7 +539,7 @@ class UI(object):
         assert cancel is None or cancel in choices.values()
         assert msg_position in ['left', 'above']
 
-        fut = asyncio.Future()  # Create a returned future
+        fut = asyncio.get_event_loop().create_future()  # Create a returned future
         oldroot = self.mainloop.widget
 
         def select_or_cancel(text):
