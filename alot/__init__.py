a5d956afcdb01f762ceb36943b4416d09aa8638a
Patrick Totzke <patricktotzke@gmail.com>
2018-12-10 14:31:43 +0000

    bump version string

--- alot/__init__.py
+++ alot/__init__.py
@@ -1,5 +1,5 @@
 __productname__ = 'alot'
-__version__ = '0.7'
+__version__ = '0.8'
 __copyright__ = "Copyright (C) 2012-18 Patrick Totzke"
 __author__ = "Patrick Totzke"
 __author_email__ = "patricktotzke@gmail.com"
