612d590b82361c39d79ccd611c8d45e30a754fb3
Chloé Dequeker <noreply@nelyah.eu>
2019-10-10 15:22:57 +0200

    feat: [ColourText] only return a value if the setting parse_quotes is True and quotes were found

--- alot/widgets/colour_text.py
+++ alot/widgets/colour_text.py
@@ -1,43 +1,59 @@
 import logging
 import re
+
+from urwid import AttrSpec
 from ..settings.const import settings
 
+
 def parse_text_colour(line):
-    """Return colour attribute as (attr, attr_focus)
+    """Get colour attribute for the line
 
-    :line: line of text to be parsed
-    :returns: attr
-
+    :param str line: line of text to be parsed
+    :return: The theme attribute to apply
     """
-    return parse_quotes(line)
+    if settings.get('parse_quotes'):
+        return parse_quotes(line)
 
 
-def parse_quotes(line, max_quote_level=7):
-    """Search for quotes. 
+def parse_quotes(line):
+    """Search for quotes.
+    Only search up to the 7th quote level.
 
-    :returns: attr, None if no quote are available
-
+    :param str line: The line of text to be parsed
+    :return: Theming attribute, None if no quote are available
     """
-    default_colour = settings.get_theming_attribute('thread', 'body')
-    is_quoted = False
-    quote_colour = None
-    symbol = settings.get('quote_symbol')
-    if symbol is None:
-        symbol = r'>'
-    for quote_level in range(1, max_quote_level+1):
-        quote_regex = r'^\ *(' + symbol + '\ *){' + str(quote_level) + r'}'
 
-        if re.match(quote_regex, line):
-            is_quoted = True
-            logging.info(f'Requesting attribute quote_level_{quote_level}')
-            quote_colour_tmp = settings.get_theming_attribute('thread', f'quote_level_{quote_level}')
+    # The value is arbitrarily set because we need to define
+    # the corresponding attributes in the theming configuration spec.
+    max_quote_level = 7
+    quote_colour = get_quote_colour(line, max_quote_level)
 
-            if quote_colour_tmp is not None:
-                quote_colour = quote_colour_tmp
-        else:
-        # If there is no match at some point, we just use the last level match colour
-            break
-    if is_quoted and quote_colour is not None:
+    if isinstance(quote_colour, AttrSpec):
         return quote_colour
     else:
-        return default_colour
+        return None
+
+
+def get_quote_colour(line, max_quote_level):
+    """Cycle through quotation levels for the line
+
+    :param str line: The line of text to be parsed
+    :param int max_quote_level: Search for quotes up to that level
+    :return: quote_colour (either string 'default' or an AttrSpec object)
+    """
+    symbol = settings.get('quote_symbol')
+    quote_colour = None
+
+    for quote_level in range(1, max_quote_level+1):
+        quote_regex = r'^ *({} *){{{}}}'.format(symbol, quote_level)
+        if re.match(quote_regex, line):
+            logging.debug(
+                'Requesting attribute quote_level_{}'.format(quote_level))
+            quote_colour = settings.get_theming_attribute(
+                'thread', 'quote_level_{}'.format(quote_level))
+
+        else:
+            # If there is no match at some point,
+            # we simply use the last level match colour
+            break
+    return quote_colour
