85858de17c5fe821ee189aa11d594e0315d47705
Matthew Richardson <M.Richardson@ed.ac.uk>
2020-07-04 12:18:50 +0100

    Update dependency to urwidtrees >= 1.0.3

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
     install_requires=[
         'notmuch>=0.27',
         'urwid>=1.3.0',
-        'urwidtrees>=1.0',
+        'urwidtrees>=1.0.3',
         'twisted>=18.4.0',
         'python-magic',
         'configobj>=4.7.0',
