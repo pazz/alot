fcdc0a93e484e326494cd4b4583a81d751632e30
Lucas Hoffmann <lucc@posteo.de>
2023-08-27 11:05:30 +0200

    Add missing import
    This was left out in #1625.

--- alot/utils/configobj.py
+++ alot/utils/configobj.py
@@ -2,6 +2,7 @@
 # This file is released under the GNU GPL, version 3 or a later revision.
 # For further details see the COPYING file
 import mailbox
+import os
 import re
 from urllib.parse import urlparse
 
