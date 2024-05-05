99163fb75b142bfec92eb3952e53a2f2ffe2a103
Lucas Hoffmann <lucc@posteo.de>
2024-05-05 04:51:49 +0200

    Patch test that breaks in the nix sandbox

--- flake.nix
+++ flake.nix
@@ -33,7 +33,13 @@
             });
 
             nativeCheckInputs = with pkgs; [ gnupg notmuch procps ];
-            checkPhase = "python3 -m unittest -v";
+            checkPhase = ''
+              # In the nix sandbox stdin is not a terminal but /dev/null so we
+              # change the shell command only in this specific test.
+              sed -i '/test_no_spawn_no_stdin_attached/,/^$/s/test -t 0/sh -c "[ $(wc -l) -eq 0 ]"/' tests/commands/test_global.py
+
+              python3 -m unittest -v
+            '';
           };
           default = self.packages.${system}.alot;
         };
