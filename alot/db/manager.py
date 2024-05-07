pazz/alot

commit 023cf168adffc5ac80e40f0e5e75f02a8c431750

alot/settings: Use notmuch config to read configuration
The main benefit from this is that we get the defaults directly from
notmuch and do not risk using inconsistent configuration values.

A good example of this is getting the database path. Getting the path
directly from the notmuch config file is not enough, because the user
might be using the default location with no explicit entry in the file.
Since notmuch does is a bit of logic notmuch to get the database path
(see section "DATABASE LOCATION" in man page for notmuch-config(1)), it
is better to get it directly from "notmuch config".

Since get_notmuch_setting() now always returns a string, also refactor
the call to settings.get_notmuch_setting('maildir', 'synchronize_flags')
for this new reality.

alot/db/manager.py
@@ -72,7 +72,7 @@ class DBManager:
             raise DatabaseROError()
         if self.writequeue:
             # read notmuch's config regarding imap flag synchronization
-            sync = settings.get_notmuch_setting('maildir', 'synchronize_flags')
+            sync = settings.get_notmuch_setting('maildir', 'synchronize_flags') == 'true'
 
             # go through writequeue entries
             while self.writequeue:

alot/settings/utils.py
@@ -9,6 +9,7 @@ from validate import Validator
 from urwid import AttrSpec
 
 from .errors import ConfigError
+from ..helper import call_cmd
 
 
 def read_config(configpath=None, specpath=None, checks=None,
@@ -79,6 +80,40 @@ def read_config(configpath=None, specpath=None, checks=None,
                 else:
                     msg.append(str(val))
             logging.info('\n'.join(msg))
+    return config
+
+
+def read_notmuch_config(path):
+    """
+    Read notmuch configuration.
+
+    This function calls the command "notmuch --config {path} config list" and
+    parses its output into a ``config`` dictionary, which is then returned.
+
+    The configuration value for a key under a section can be accessed with
+    ``config[section][key]``.
+
+    The returned value is a dict ``config`` with
+
+    :param path: path to the configuration file, which is passed as
+        argument to the --config option of notmuch.
+    :type path: str
+    :raises: :class:`~alot.settings.errors.ConfigError`
+    :rtype: `dict`
+    """
+    cmd = ['notmuch', '--config', path, 'config', 'list']
+    out, err, code = call_cmd(cmd)
+    if code != 0:
+        msg = f'failed to read notmuch config with command {cmd} (exit error: {code}):\n{err}'
+        logging.error(msg)
+        raise ConfigError(msg)
+
+    config = {}
+    for line in out.splitlines():
+        left_hand, right_hand = line.split("=", maxsplit=1)
+        section, key = left_hand.split(".", maxsplit=1)
+        config.setdefault(section, {})[key] = right_hand
+
     return config
 
 

alot/settings/manager.py
@@ -17,7 +17,7 @@ from ..helper import pretty_datetime, string_decode, get_xdg_env
 from ..utils import configobj as checks
 
 from .errors import ConfigError, NoMatchingAccount
-from .utils import read_config
+from .utils import read_config, read_notmuch_config
 from .utils import resolve_att
 from .theme import Theme
 
@@ -36,12 +36,13 @@ class SettingsManager:
         self._accounts = None
         self._accountmap = None
         self._notmuchconfig = None
+        self._notmuchconfig_path = None
         self._config = ConfigObj()
         self._bindings = None
 
     def reload(self):
         """Reload notmuch and alot config files"""
-        self.read_notmuch_config(self._notmuchconfig.filename)
+        self.read_notmuch_config(self._notmuchconfig_path)
         self.read_config(self._config.filename)
 
     def read_notmuch_config(self, path):
@@ -50,8 +51,8 @@ class SettingsManager:
         :param path: path to notmuch's config file
         :type path: str
         """
-        spec = os.path.join(DEFAULTSPATH, 'notmuch.rc.spec')
-        self._notmuchconfig = read_config(path, spec)
+        self._notmuchconfig = read_notmuch_config(path)
+        self._notmuchconfig_path = path  # Set the path *after* succesful read.
 
     def _update_bindings(self, newbindings):
         assert isinstance(newbindings, Section)
@@ -275,7 +276,8 @@ class SettingsManager:
         :type key: str
         :param fallback: fallback returned if key is not present
         :type fallback: str
-        :returns: config value with type as specified in the spec-file
+        :returns: the config value
+        :rtype: str
         """
         value = None
         if section in self._notmuchconfig:

alot/defaults/notmuch.rc
@@ -1,3 +1,0 @@
-[maildir]
-synchronize_flags = False
-

alot/defaults/notmuch.rc.spec
@@ -1,3 +1,0 @@
-[maildir]
-synchronize_flags = boolean(default=False)
-

setup.py
@@ -31,7 +31,6 @@ setup(
     package_data={
         'alot': [
             'defaults/alot.rc.spec',
-            'defaults/notmuch.rc.spec',
             'defaults/abook_contacts.spec',
             'defaults/default.theme',
             'defaults/default.bindings',

commit 7a82785eb69f6294d417ff807cd1fbb4d1a984b2

Move validation of notmuch config values to read_notmuch_config
The related test for the config validation of "synchronize_flags" in
tests/settings/test_manager.py was broken since 023cf168, where the
parsing of the notmuch config file was migrated from the python
configobj based parsing function to loading the notmuch settings
including defaults from the notmuch CLI.

.github/workflows/test.yml
@@ -60,10 +60,9 @@ jobs:
         run: >
           sed -Ei
           -e '1iimport unittest'
-          -e 's/^(\s*)(async )?def test_(no_spawn_no_stdin_attached|save_named_query|parsing_notmuch_config_with_non_bool_synchronize_flag_fails)/\1@unittest.skip("broken in ci")\n&/'
+          -e 's/^(\s*)(async )?def test_(no_spawn_no_stdin_attached|save_named_query)/\1@unittest.skip("broken in ci")\n&/'
           tests/commands/test_global.py
           tests/db/test_manager.py
-          tests/settings/test_manager.py
 
       - name: Run tests
         run: python3 -m unittest --verbose

alot/db/manager.py
@@ -51,9 +51,7 @@ class DBManager:
         if exclude_tags is not None:
             return exclude_tags
 
-        exclude_tags = settings.get_notmuch_setting('search', 'exclude_tags')
-        if exclude_tags:
-            return [t for t in exclude_tags.split(';') if t]
+        return settings.get_notmuch_setting('search', 'exclude_tags')
 
     def flush(self):
         """
@@ -72,7 +70,7 @@ class DBManager:
             raise DatabaseROError()
         if self.writequeue:
             # read notmuch's config regarding imap flag synchronization
-            sync = settings.get_notmuch_setting('maildir', 'synchronize_flags') == 'true'
+            sync = settings.get_notmuch_setting('maildir', 'synchronize_flags')
 
             # go through writequeue entries
             while self.writequeue:

alot/settings/utils.py
@@ -93,13 +93,15 @@ def read_notmuch_config(path):
     The configuration value for a key under a section can be accessed with
     ``config[section][key]``.
 
-    The returned value is a dict ``config`` with
+    The returned value is a dict ``config`` with some values converted to
+    special python types.  These are the values that alot is known to use.  All
+    other values in the returned dict are just strings.
 
     :param path: path to the configuration file, which is passed as
         argument to the --config option of notmuch.
     :type path: str
     :raises: :class:`~alot.settings.errors.ConfigError`
-    :rtype: `dict`
+    :rtype: dict
     """
     cmd = ['notmuch', '--config', path, 'config', 'list']
     out, err, code = call_cmd(cmd)
@@ -112,6 +114,23 @@ def read_notmuch_config(path):
     for line in out.splitlines():
         left_hand, right_hand = line.split("=", maxsplit=1)
         section, key = left_hand.split(".", maxsplit=1)
+        if section == "maildir" and key == "synchronize_flags":
+            if right_hand == "true":
+                right_hand = True
+            elif right_hand == "false":
+                right_hand = False
+            else:
+                raise ConfigError(
+                    f"Bad value for maildir.synchronize_flags: {right_hand}")
+        if section == "search" and key == "exclude_tags":
+            try:
+                if right_hand:
+                    right_hand = [t for t in right_hand.split(';') if t]
+                else:
+                    right_hand = None
+            except:
+                raise ConfigError(
+                    "Can not split search.exclude_tags into a list: {right_hand}")
         config.setdefault(section, {})[key] = right_hand
 
     return config