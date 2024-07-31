8853c7a82a720fd25f874ce6d9f9eba6130bf8e3
Patrick Totzke <patrick@totzke.uk>
2024-07-31 15:34:06 +0100

    pull alot.__version__ from importlib metadata
    .. dynamically and remove some other metadata from alot/__init__.py to
    avoid duplication
    
    This is used in `alot -v` but also when setting `UserAgent` for
    outgoing mails

--- alot/__init__.py
+++ alot/__init__.py
@@ -1,8 +1,17 @@
+from importlib.metadata import version, PackageNotFoundError
+
+# this requires python >=3.8
+try:
+    __version__ = version("alot")
+except PackageNotFoundError:
+    # package is not installed
+    pass
+
+
 __productname__ = 'alot'
-__version__ = '0.10'
-__copyright__ = "Copyright (C) 2012-21 Patrick Totzke"
-__author__ = "Patrick Totzke"
-__author_email__ = "patricktotzke@gmail.com"
+# -__copyright__ = "Copyright (C) 2012-21 Patrick Totzke"
+# __author__ = "Patrick Totzke"
+# __author_email__ = "patricktotzke@gmail.com"
 __description__ = "Terminal MUA using notmuch mail"
-__url__ = "https://github.com/pazz/alot"
-__license__ = "Licensed under the GNU GPL v3+."
+# __url__ = "https://github.com/pazz/alot"
+# __license__ = "Licensed under the GNU GPL v3+."
