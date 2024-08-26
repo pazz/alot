63bfaf16112b65f144e241156f2d1e09be2b828e
Patrick Totzke <patricktotzke@gmail.com>
2024-08-26 15:25:56 +0100

    cleanups: remove some comments

--- alot/__init__.py
+++ alot/__init__.py
@@ -9,9 +9,4 @@ except PackageNotFoundError:
 
 
 __productname__ = 'alot'
-# -__copyright__ = "Copyright (C) 2012-21 Patrick Totzke"
-# __author__ = "Patrick Totzke"
-# __author_email__ = "patricktotzke@gmail.com"
 __description__ = "Terminal MUA using notmuch mail"
-# __url__ = "https://github.com/pazz/alot"
-# __license__ = "Licensed under the GNU GPL v3+."
